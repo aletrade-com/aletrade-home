7211b10e50a156b56e5565008f4d7a207cd33a85
aletrade-com <info@aletrade.com>
2025-07-12 12:31:59 +0200

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <section className="py-10 px-6 text-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        <span className="inline-flex items-center gap-2">
-          Bienvenido a 
-          <img src="/logo-horizontal.png" alt="Aletrade" className="h-8 inline-block" />
-        </span>
-      </h1>
+      <h1 className="text-xl sm:text-2xl font-bold text-blue-600 text-center">
+  <span className="inline-flex items-center gap-1 justify-center">
+    Bienvenido a
+    <img src="/logo-horizontal.png" alt="Aletrade" className="h-16 sm:h-20 inline-block" />
+  </span>
+</h1>
 
       <p className="text-xl mt-4 text-gray-800">
         Tu asistente para el control de <strong>finanzas personales y profesionales</strong>.
       </p>
 
       <div className="mt-6">
-        <a
-          href="/recursos"
-          className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition"
-        >
-          Ver recursos disponibles
-        </a>
+        <Link
+  to="/recursos"
+  className="mt-6 inline-block bg-blue-600 text-white font-semibold py-2 px-6 rounded hover:bg-blue-700 transition"
+>
+  Ver recursos disponibles
+</Link>
       </div>
     </section>
   );
