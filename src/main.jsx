427d92823a49195aee3a81ce5d18b54ab3468109
aletrade-com <info@aletrade.com>
2025-07-22 18:08:06 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
+
+// 🔁 Redirección desde ?redirect=/ruta
+const redirect = new URLSearchParams(window.location.search).get("redirect");
+if (redirect && window.location.pathname === "/") {
+  window.history.replaceState(null, "", redirect);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
